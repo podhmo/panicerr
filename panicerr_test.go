3c5522567061045cc3833cd4f06648fa454bf0c5
podhmo <ababjam61+github@gmail.com>
2021-03-10 22:16:57 +0900

    test: more tests

--- panicerr_test.go
+++ panicerr_test.go
@@ -1,9 +1,12 @@
 package panicerr
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"testing"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func TestSimple(t *testing.T) {
@@ -28,5 +31,22 @@ func TestSimplePanicWithError(t *testing.T) {
 	}
 	if !errors.Is(err, this) {
 		t.Errorf("expected error is %[1]T:%[1]v, but got %[2]T:%[2]v, unwrap is failed", this, err)
+	}
+}
+func TestSimpleWithGoroutine(t *testing.T) {
+	g, _ := errgroup.WithContext(context.Background())
+	g.Go(func() (err error) {
+		defer Recoverer(&err)
+		t.Log("ok")
+		return nil
+	})
+	g.Go(func() (err error) {
+		defer Recoverer(&err)
+		t.Log("ng")
+		panic("hmm")
+	})
+	err := g.Wait()
+	if err == nil {
+		t.Errorf("expected error, but nil, something wrong")
 	}
 }
